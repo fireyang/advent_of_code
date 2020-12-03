b8d7eb23af39bca4ebcea809125cd9209d306959
fireyang <shenhuan007@gmail.com>
2020-12-03 14:50:15 +0800

    box

--- src/day3.rs
+++ src/day3.rs
@@ -2,7 +2,7 @@
 mod day3 {
     use std::collections::HashMap;
 
-    fn get_map(vec: &Vec<String>) -> (i32, i32, HashMap<(usize, usize), char>){
+    fn get_map(vec: Box<Vec<String>>) -> (i32, i32, HashMap<(usize, usize), char>){
         // let mut map = HashMap::new();
         let mut w:usize = 0;
         // let mut h = 0;
@@ -33,11 +33,11 @@ mod day3 {
 
 
     #[allow(dead_code)]
-    pub fn part1(vec: Vec<String>, mov: (i32, i32)) -> i32 {
+    pub fn part1(vec: Box<Vec<String>>, mov: (i32, i32)) -> i32 {
         // let mut start_post = (1,1);
         let mut x:i32 = 0;
         let mut y:i32 = 0;
-        let (w, h, map) = get_map(&vec);
+        let (w, h, map) = get_map(vec);
         // println!("{:?}",(w, h, map.len()));
         let (mx, my) = mov;
         let mut count = 0;
@@ -90,7 +90,7 @@ mod tests {
     fn day3_part1() {
         let list = common::parse_from_file("./data/day3_part1.txt");
         // let list = common::parse_from_file("./data/day3_test.txt");
-        let result = day3::part1(list.unwrap(), (3, 1));
+        let result = day3::part1(Box::new(list.unwrap()), (3, 1));
         assert_eq!(result, 216);
     }
 
@@ -100,8 +100,9 @@ mod tests {
         let list = common::parse_from_file("./data/day3_part1.txt");
         let move_list = vec![(1,1), (3,1), (5,1), (7,1), (1,2)];
         let mut count:i64 = 1;
+        let list = Box::new(list.unwrap());
         for it in move_list{
-            let v = day3::part1(list.clone().unwrap(), it);
+            let v = day3::part1(list.clone(), it);
             // println!("out:{:?}", (it, v, count));
             count *= v as i64;
         }
