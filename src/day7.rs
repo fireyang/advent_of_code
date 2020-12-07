2ccbfcacd9ed81570962a4fba59e9bb04bcfc0a1
fireyang <shenhuan007@gmail.com>
2020-12-07 21:49:02 +0800

    day7 part2

--- src/day7.rs
+++ src/day7.rs
@@ -5,10 +5,14 @@ mod day7 {
 
     use std::collections::HashMap;
     use std::collections::HashSet;
-    // use regex::Regex;
 
-    fn gen_map(vec: Box<Vec<String>>) -> HashMap<String, Vec<String>>{
-        let mut map:HashMap<String, Vec<String>>= HashMap::new();
+    #[derive(Clone, Debug)]
+    struct Bag{
+        num: i32,
+        color: String,
+    }
+
+    fn _gen_deps(vec: Box<Vec<String>>) -> Vec<(String, Vec<(i32, String)>)> {
         let chan: Vec<_> = vec.iter()
             .map(|x|{
                 let x = x.replace("bags", "").replace("bag", "").replace(".", "");
@@ -33,9 +37,14 @@ mod day7 {
                     .collect();
                 (x1.to_string(), bags)
             }).collect();
+        chan
+    }
+
+    fn _belong_map(vec: Box<Vec<String>>) -> HashMap<String, Vec<String>>{
+        let mut map:HashMap<String, Vec<String>>= HashMap::new();
+        let chan: Vec<_> = _gen_deps(vec);
         for it in chan.iter(){
             let (color1, bags) = it;
-            // println!("{:?}", it);
             for it2 in bags.iter(){
                 match it2 {
                     (0, _) =>(),
@@ -45,26 +54,62 @@ mod day7 {
                     }
                 }
             }
-            // println!("{:?}", (num, color));
+        }
+        map
+    }
+
+    fn _include_map(vec: Box<Vec<String>>) -> HashMap<String, Vec<Bag>>{
+        let mut map:HashMap<String, Vec<Bag>>= HashMap::new();
+        let chan: Vec<_> = _gen_deps(vec);
+        for it in chan.iter(){
+            let (color1, bags) = it;
+            for it2 in bags.iter(){
+                match it2 {
+                    (0, _) =>(),
+                    (num, color) => {
+                        let list  =  map.entry(color1.to_string()).or_insert(Vec::new());
+                        let bag = Bag{num: *num , color: color.to_string()};
+                        list.push(bag);
+                    }
+                }
+            }
         }
         map
     }
 
     fn get_parent_num(map: Box<HashMap<String, Vec<String>>>, key: &String, out:Rc<RefCell<HashSet<String>>>) {
-        if let Some(keys) = map.get(key){
-            keys.iter().for_each(|it| get_parent_num(map.clone(), it, out.clone()));
-            for it in keys.iter(){
+        if let Some(bags) = map.get(key){
+            bags.iter().for_each(|it| {
                 out.borrow_mut().insert(it.to_string());
-            }
+                get_parent_num(map.clone(), &it, out.clone());
+            });
         }
+    }
+
+    fn _get_child_num(map: Rc<HashMap<String, Vec<Bag>>>, key: &String) -> i32{
+        let mut count = 0;
+        if let Some(bags) = map.get(key){
+            bags.iter().for_each(|it| {
+                let v = _get_child_num(map.clone(), &it.color);
+                count += v * it.num + it.num;
+            });
+        }
+        count
     }
 
     #[allow(dead_code)]
     pub fn part1(vec: Box<Vec<String>>) -> i32 {
         let set: Rc<RefCell<_>> = Rc::new(RefCell::new(HashSet::new()));
-        let map = gen_map(vec);
+        let map = _belong_map(vec);
         get_parent_num(Box::new(map), &"shiny gold".to_string(), set.clone());
         let v = set.borrow().len() as i32;
+        v
+    }
+
+    #[allow(dead_code)]
+    pub fn part2(vec: Box<Vec<String>>, bag: &String) -> i32 {
+        let map = _include_map(vec);
+        let v = _get_child_num(Rc::new(map), bag);
         v
     }
 
@@ -81,30 +126,16 @@ mod tests {
         let list = common::parse_from_file("./data/day7_part1.txt");
         let v = day7::part1(Box::new(list.unwrap()));
 
-        assert_eq!(v, 871);
+        assert_eq!(v, 337);
     }
 
-    // #[test]
-    // fn day7_part2() {
-    //     let list = common::parse_from_file("./data/day7_part1.txt");
-    //     let mut vec = Vec::new();
-    //     for it in list.unwrap(){
-    //         let v = day7::part1(it.clone());
-    //         vec.push(v);
-    //     }
-    //     vec.sort();
-    //     let mut cur = 0;
-    //     for val in vec.iter(){
-    //         if cur !=0 && cur +1 != *val{
-    //             break;
-    //         }else{
-    //             cur = *val;
-    //         }
-    //     }
-    //     cur += 1;
-    //     println!("out:{:?}", cur);
-    //
-    //     assert_eq!(cur, 640);
-    // }
+    #[test]
+    fn day7_part2() {
+        let list = common::parse_from_file("./data/day7_part1.txt");
+        // let list = common::parse_from_file("./data/day7_test.txt");
+        let v = day7::part2(Box::new(list.unwrap()), &"shiny gold".to_string());
+
+        assert_eq!(v, 871);
+    }
 
 }
