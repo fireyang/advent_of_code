bf048e599d93eb24978b01c9670389ebbd386479
fireyang <shenhuan007@gmail.com>
2020-12-11 16:28:49 +0800

    day part1 vector

--- src/day11.rs
+++ src/day11.rs
@@ -1,101 +1,98 @@
 mod day11 {
-    use std::collections::HashMap;
-    use std::rc::Rc;
 
     #[allow(dead_code)]
     pub fn part1(vec: Box<Vec<String>>) -> i32 {
-        println!("hello");
-        let mut map: HashMap<(usize, usize), char> = HashMap::new();
-        let mut width = 0;
-        vec.iter().enumerate().for_each(|(y, it)| {
-            width = it.len();
-            it.chars().enumerate().for_each(|(x, it2)| {
-                let mut c = it2;
-                if c == 'L' {
-                    c = '#';
-                }
-                // println!("it2:{:?}", (x, y, it2));
-                map.insert((x, y), c);
+        // let mut map: HashMap<(usize, usize), char> = HashMap::new();
+        // let mut grids = vec![];
+        let grids: Vec<Vec<char>> = vec
+            .iter()
+            .map(|it| {
+                let row = it.chars().collect();
+                row
             })
-        });
+            .collect();
         // println!("map, {:?}", map);
         let mut change = true;
         let mut change_num = 0;
-        let mut map = Rc::new(map);
+        let mut map = grids;
         while change {
-            // _print_map(map.clone(), width, "aaa");
-            let (c, new_map) = change_pos(map.clone());
-            // println!("cc, {:?}", (change_num, c));
+            let (c, new_map) = change_pos(&map);
             change = c;
-            map = Rc::new(new_map);
+            map = new_map;
             change_num += 1;
-            // _print_map(map.clone(), width, "bbb");
+            // _print_map(&map, "bbb");
         }
-        let c = map.iter().map(|(_, v)| v).filter(|&x| x == &'#').count();
+        let c = map.iter().flatten().filter(|&x| x == &'#').count();
         println!("map2, {:?}", (change_num, c));
-        0
+        c as i32
     }
 
-    fn _print_map(map: Rc<HashMap<(usize, usize), char>>, w: usize, key: &str) {
+    fn _print_map(v: &Vec<Vec<char>>, key: &str) {
         println!("==========={:?}===========", key);
-        for i in 0..w {
-            let mut v = vec![];
-            for j in 0..map.len() / w {
-                v.push(map[&(j, i)]);
-            }
-            let s: String = v.into_iter().collect();
+        for it in v.iter() {
+            let s: String = it.into_iter().collect();
             println!("{:?}", s);
         }
         println!("======================");
     }
 
-    pub fn change_pos(
-        map: Rc<HashMap<(usize, usize), char>>,
-    ) -> (bool, HashMap<(usize, usize), char>) {
-        let mut new_map: HashMap<(usize, usize), char> = HashMap::new();
-        let mut all_change = false;
-        for ((x, y), c1) in map.iter() {
-            if c1 == &'.' {
-                new_map.insert((*x, *y), *c1);
-                continue;
-            }
-            let mut adjacent = vec![];
-            for i in 0..3 {
-                for j in 0..3 {
-                    if i == j && i == 1 {
-                        continue;
-                    }
-                    if x + i == 0 || y + j == 0 {
-                        continue;
-                    }
-                    let px = x + i - 1;
-                    let py = y + j - 1;
-                    if let Some(c2) = map.get(&(px, py)) {
-                        if c2 != &'.' {
-                            adjacent.push(*c2);
-                        }
+    fn get_adjacent(vec: &Vec<Vec<char>>, x: usize, y: usize) -> Vec<char> {
+        let mut adjacent = vec![];
+        for i in 0..3 {
+            for j in 0..3 {
+                if i == j && i == 1 {
+                    continue;
+                }
+                if x + i == 0 || y + j == 0 {
+                    continue;
+                }
+                let px = x + i - 1;
+                let py = y + j - 1;
+                if let Some(r) = vec.get(py) {
+                    if let Some(v) = r.get(px) {
+                        adjacent.push(*v);
                     }
                 }
             }
-            let change: bool;
-            if c1 == &'L' {
-                change = adjacent.iter().all(|x| x == &'L');
-            } else {
-                change = adjacent.iter().filter(|&x| x == &'#').count() > 3;
-            }
-
-            let mut new_c = *c1;
-            if change {
-                if new_c == '#' {
-                    new_c = 'L';
-                } else {
-                    new_c = '#';
-                }
-                all_change = true;
-            }
-            new_map.insert((*x, *y), new_c);
         }
-        (all_change, new_map)
+        adjacent
+    }
+
+    pub fn change_pos(grids: &Vec<Vec<char>>) -> (bool, Vec<Vec<char>>) {
+        let mut new_grids = grids.clone();
+        let mut all_change = false;
+        for (y, row) in grids.iter().enumerate() {
+            for x in 0..row.len() {
+                let c1 = grids[y][x];
+                if c1 == '.' {
+                    continue;
+                }
+                let adjacent = get_adjacent(&grids, x, y);
+                let change: bool;
+                if c1 == 'L' {
+                    change = adjacent.iter().filter(|&x| x != &'.').all(|x| x == &'L');
+                } else {
+                    change = adjacent
+                        .iter()
+                        .filter(|&x| x != &'.')
+                        .filter(|&x| x == &'#')
+                        .count()
+                        > 3;
+                }
+
+                let mut new_c = c1;
+                if change {
+                    if new_c == '#' {
+                        new_c = 'L';
+                    } else {
+                        new_c = '#';
+                    }
+                    all_change = true;
+                }
+                new_grids[y][x] = new_c;
+            }
+        }
+        (all_change, new_grids)
     }
 }
 
@@ -111,6 +108,6 @@ mod tests {
         let v = day11::part1(Box::new(list.unwrap()));
         // let list = common::parse_from_file("./data/day11_part1.txt");
         // let v = day11::part1(Box::new(list.unwrap()), 25);
-        assert_eq!(v, 2484);
+        assert_eq!(v, 2104);
     }
 }
