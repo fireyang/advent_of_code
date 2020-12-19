a6b769939ad8d716c7ac49b16ac3d1491e4c67ac
fireyang_ali <shenhuan007@gmail.com>
2020-12-19 16:09:39 +0800

    day17 part2

--- src/day17.rs
+++ src/day17.rs
@@ -1,7 +1,7 @@
 mod day17 {
     use std::collections::HashMap;
 
-    type Position = (i32, i32, i32);
+    type Position = (i32, i32, i32, i32);
     type Range = (i32, i32);
 
     #[allow(dead_code)]
@@ -14,16 +14,16 @@ mod day17 {
                     _ => 1,
                 };
                 if v > 0 {
-                    grid.insert((x as i32, y as i32, 0), v);
+                    grid.insert((x as i32, y as i32, 0, 0), v);
                 }
             });
         });
         println!("{:?}", grid);
-        // print_grid(&grid);
+        print_grid(&grid);
         for i in 0..6 {
             println!("turn {}", i);
             grid = turn_one(&grid);
-            // print_grid(&new_grid);
+            // print_grid(&grid);
         }
         println!("out {}", grid.len());
         grid.len() as i32
@@ -31,19 +31,21 @@ mod day17 {
 
     fn turn_one(grid: &HashMap<Position, i32>) -> HashMap<Position, i32> {
         let mut new_grid: HashMap<Position, i32> = HashMap::new();
-        let (x_r, y_r, z_r) = get_grid_range(&grid);
-        for z in z_r.0..z_r.1 + 3 {
-            for y in y_r.0..y_r.1 + 3 {
-                for x in x_r.0..x_r.1 + 3 {
-                    let pos = (x - 1 as i32, y - 1 as i32, z - 1 as i32);
-                    let num = get_active_num(&grid, &pos);
-                    // println!("{:?}", (pos, num));
-                    if let Some(_) = grid.get(&pos) {
-                        if num == 2 || num == 3 {
+        let (x_r, y_r, z_r, w_r) = get_grid_range(&grid);
+        for w in w_r.0..w_r.1 + 3 {
+            for z in z_r.0..z_r.1 + 3 {
+                for y in y_r.0..y_r.1 + 3 {
+                    for x in x_r.0..x_r.1 + 3 {
+                        let pos = (x as i32 - 1, y as i32 - 1, z as i32 - 1, w as i32 - 1);
+                        let num = get_active_num(&grid, &pos);
+                        // println!("{:?}", (pos, num));
+                        if let Some(_) = grid.get(&pos) {
+                            if num == 2 || num == 3 {
+                                new_grid.insert(pos, 1);
+                            }
+                        } else if num == 3 {
                             new_grid.insert(pos, 1);
                         }
-                    } else if num == 3 {
-                        new_grid.insert(pos, 1);
                     }
                 }
             }
@@ -51,16 +53,18 @@ mod day17 {
         new_grid
     }
 
-    fn get_grid_range(grid: &HashMap<Position, i32>) -> (Range, Range, Range) {
+    fn get_grid_range(grid: &HashMap<Position, i32>) -> (Range, Range, Range, Range) {
         let mut x_range = (i32::MAX, i32::MIN);
         let mut y_range = (i32::MAX, i32::MIN);
         let mut z_range = (i32::MAX, i32::MIN);
+        let mut w_range = (i32::MAX, i32::MIN);
         grid.iter().for_each(|(k, _v)| {
             x_range = get_range(x_range, k.0);
             y_range = get_range(y_range, k.1);
             z_range = get_range(z_range, k.2);
+            w_range = get_range(w_range, k.3);
         });
-        (x_range, y_range, z_range)
+        (x_range, y_range, z_range, w_range)
     }
 
     fn get_active_num(grid: &HashMap<Position, i32>, pos: &Position) -> i32 {
@@ -82,12 +86,15 @@ mod day17 {
         for i in 0..3 {
             for j in 0..3 {
                 for k in 0..3 {
-                    let x = pos.0 + i;
-                    let y = pos.1 + j;
-                    let z = pos.2 + k;
-                    let new_pos = (x as i32 - 1, y as i32 - 1, z as i32 - 1);
-                    if &new_pos != pos {
-                        list.push(new_pos);
+                    for n in 0..3 {
+                        let x = pos.0 + i;
+                        let y = pos.1 + j;
+                        let z = pos.2 + k;
+                        let w = pos.3 + n;
+                        let new_pos = (x as i32 - 1, y as i32 - 1, z as i32 - 1, w as i32 - 1);
+                        if &new_pos != pos {
+                            list.push(new_pos);
+                        }
                     }
                 }
             }
@@ -97,19 +104,21 @@ mod day17 {
 
     #[allow(dead_code)]
     fn print_grid(grid: &HashMap<Position, i32>) {
-        let (x_range, y_range, z_range) = get_grid_range(&grid);
-        for z in z_range.0..z_range.1 + 1 {
-            println!("z={:?}", z);
-            for y in y_range.0..y_range.1 + 1 {
-                let mut s = String::from("");
-                for x in x_range.0..x_range.1 + 1 {
-                    if let Some(_) = grid.get(&(x, y, z)) {
-                        s.push('#');
-                    } else {
-                        s.push('.');
+        let (x_r, y_r, z_r, w_r) = get_grid_range(&grid);
+        for w in w_r.0..w_r.1 + 1 {
+            for z in z_r.0..z_r.1 + 1 {
+                println!("z={:?}, w={}", z, w);
+                for y in y_r.0..y_r.1 + 1 {
+                    let mut s = String::from("");
+                    for x in x_r.0..x_r.1 + 1 {
+                        if let Some(_) = grid.get(&(x, y, z, w)) {
+                            s.push('#');
+                        } else {
+                            s.push('.');
+                        }
                     }
+                    println!("{}", s);
                 }
-                println!("{}", s);
             }
         }
     }
@@ -136,6 +145,6 @@ mod tests {
         // let list = common::parse_from_file("./data/day17_test.txt");
         let list = common::parse_from_file("./data/day17_part1.txt");
         let v = day17::part1(list.unwrap());
-        assert_eq!(v, 293);
+        assert_eq!(v, 1816);
     }
 }
